cd7b60ee6073300b5db286b17bffb8fcef65b194
francis <franzdiaz460@gmail.com>
2024-07-13 21:42:58 +0400

    ref: setting up pages

--- src/routes/auth/Auth.tsx
+++ src/routes/auth/Auth.tsx
@@ -1,18 +1,20 @@
-import { useEffect, } from "react";
+import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 const userTest = false;
 export default function Auth(): React.ReactElement {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState<{ name: string }>({ name: "User" });
   useEffect(() => {
     // check if user is logged in or not, navigate accordingly
     if (!userTest) {
       navigate("login");
-      console.log("redirect")
+      console.log("redirect");
+      return;
     }
-  }, []);
-
-  return <div>
-    Auth Route
-    <Outlet />
-  </div>
-};
+  });
+  return (
+    <main className="main-container justify-center items-center text-3xl bg-light text text-dk-100">
+      <Outlet context={{ authForm: { formData, setFormData } }} />
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "#2B2B2B",
+      light: "#FDF9EF",
+      accent: {
+        100: "#F8EED4",
+      },
+      dk: {
+        100: "#1D1D1D",
+        200: "#2B2B2B",
+        300: "#131313",
+        400: "#333333",
+      },
+      lt: {
+        100: "#F8EED4",
+        200: "#FDF9EF",
+        300: "#ffffff",
+      },
+      lw: {
+        100: "#8A8A8A",
+        200: "#6A6A6A",
+      },
+    },
     extend: {},
   },
   plugins: [],
